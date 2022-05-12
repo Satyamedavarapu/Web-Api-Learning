b9e6fdfbdfd6ed462cd971ffd8a3df61210ed1fe
Satyamedavarapu <satya.medavarapu@gmail.com>
2022-05-12 18:09:26 +0530

    Add project files.

---
+++ Controllers/GetPlayersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web_Api_Learning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetPlayersController : ControllerBase
+    {
+
+
+        private readonly DataContext _dataContext;
+
+        public GetPlayersController(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+
+
+            if (id == 0)
+            {
+                var players = await _dataContext.PLAYERS.ToListAsync();
+                return Ok(players);
+
+            }
+            else
+            {
+                var player = await _dataContext.PLAYERS.FindAsync(id);
+                return Ok(player);
+            }
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
